c3120a42e885e98175382b031998527389b8c38d
hashikami <hashikami@pharsas.com>
2015-07-24 22:22:20 +0900

    [add] record

--- server.py
+++ server.py
@@ -60,5 +60,5 @@ def record():
   return render_template('record.html')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
-  # app.run(port=int(os.environ.get('PORT', 5000)))
+  # app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
+  app.run(port=int(os.environ.get('PORT', 5000)))
